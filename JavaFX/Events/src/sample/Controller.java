ac89c9b07c8147421ea0f6f6a919142f9b54389e
kathyebel <kathyebel@me.com>
2018-06-25 15:20:03 -0400

    initial commit

---
+++ JavaFX/Events/src/sample/Controller.java
@@ -0,0 +1,83 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
+
+public class Controller {
+    @FXML
+    private TextField nameField;
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button byeButton;
+
+    @FXML
+    private CheckBox checkBox;
+
+    @FXML
+    private Label ourLabel;
+
+    @FXML
+    public void initialize() {
+        disableButtons();
+    }
+
+
+    @FXML
+    public void onButtonClicked(ActionEvent event) {
+        if (event.getSource().equals(helloButton)) {
+            System.out.println("Hello, " + nameField.getText());
+        } else if (event.getSource().equals(byeButton)) {
+            System.out.println("Bye, " + nameField.getText());
+        }
+
+        Runnable task = () -> {
+            try {
+                String s = Platform.isFxApplicationThread() ? "UI Thread": "Background Thread";
+                System.out.println("I'm going to sleep on the: " + s);
+                Thread.sleep(10000);
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        String s = Platform.isFxApplicationThread() ? "UI Thread": "Background Thread";
+                        System.out.println("I'm updating the label on the: " + s);
+                        ourLabel.setText("We did something!");
+                    }
+                });
+            } catch (InterruptedException e) {
+                // we don't care about this
+            }
+        };
+
+        new Thread(task).start();
+        if (checkBox.isSelected()) {
+            nameField.clear();
+            disableButtons();
+        }
+    }
+
+    @FXML
+    public void handleKeyReleased() {
+        String text = nameField.getText();
+        boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
+        helloButton.setDisable(disableButtons);
+        byeButton.setDisable(disableButtons);
+    }
+
+    @FXML
+    public void handleChange() {
+        System.out.println("CheckBox is " + (checkBox.isSelected() ? "checked" : "not checked"));
+    }
+
+    private void disableButtons() {
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+    }
+}
---
+++ JavaFX/TodoList/src/com/katherineebel/todolist/Controller.java
@@ -0,0 +1,75 @@
+package com.katherineebel.todolist;
+
+import com.katherineebel.todolist.datamodel.Todo;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
+    private List<Todo> todos;
+
+    @FXML
+    private ListView<Todo> todoListView;
+    @FXML
+    private TextArea todoDetailsArea;
+    @FXML
+    private Label deadlineLabel;
+
+    public void initialize() {
+        Todo item1 = new Todo("mail birthday card",
+                "Buy a 30th birthday car for John",
+                LocalDate.of(2018, Month.JULY, 25));
+        Todo item2 = new Todo("Doctor's Appointment",
+                "See Dr Smith at 123 Main Street",
+                LocalDate.of(2018, Month.AUGUST, 10));
+        Todo item3 = new Todo("Finish design proposal for client",
+                "I promised Mike I'd email website mockups by Friday Sept 30th",
+                LocalDate.of(2018, Month.SEPTEMBER, 30));
+        Todo item4 = new Todo("Pickup Doug at train station",
+                "doug's arriving on June 27 on the 5:00 train",
+                LocalDate.of(2018, Month.JUNE, 27));
+        Todo item5 = new Todo("Pick up dry cleaning",
+                "The clothes should be ready by 2:30",
+                LocalDate.of(2018, Month.JUNE, 29));
+
+        todos = new ArrayList<>();
+        todos.add(item1);
+        todos.add(item2);
+        todos.add(item3);
+        todos.add(item4);
+        todos.add(item5);
+
+        todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                if (newValue != null) {
+                    Todo item = todoListView.getSelectionModel().getSelectedItem();
+                    todoDetailsArea.setText(item.getDetails());
+                    deadlineLabel.setText(item.getDeadline().toString());
+                }
+            }
+        });
+        todoListView.getItems().setAll(todos);
+        todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        todoListView.getSelectionModel().selectFirst();
+    }
+
+    @FXML
+    public void handleListClickListView() {
+        Todo item = (Todo) todoListView.getSelectionModel().getSelectedItem();
+        todoDetailsArea.setText(item.getDetails());
+        deadlineLabel.setText(item.getDeadline().toString());
+//        StringBuilder sb = new StringBuilder(item.getDetails());
+//        sb.append("\n\n\n\n").append("Due: ").append(item.getDeadline().toString());
+//        todoDetailsArea.setText(sb.toString());
+    }
+}
