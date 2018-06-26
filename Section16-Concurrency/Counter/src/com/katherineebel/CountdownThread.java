eab1932d4175dd4723cb99cce338e625a636e67e
kathyebel <kathyebel@me.com>
2018-06-26 17:06:08 -0400

    Start Countdown console project. complete lecture 243 multiple threads

---
+++ Section16-Concurrency/Counter/src/com/katherineebel/CountdownThread.java
@@ -0,0 +1,13 @@
+package com.katherineebel;
+
+class CountdownThread extends Thread {
+    private Countdown threadCountdown;
+
+    public CountdownThread(Countdown countdown) {
+        threadCountdown = countdown;
+    }
+
+    public void run() {
+        threadCountdown.doCountdown();
+    }
+}
